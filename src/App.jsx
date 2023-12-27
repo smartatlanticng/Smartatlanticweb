1f08833688efbfe15be06d660df04fdb701d9c7e
taotara <taotara@yahoo.com>
2023-12-27 04:04:51 +0100

    Initail project setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Insights from "./pages/insights/Insights";
+import Careers from "./pages/careers/Careers";
+import Contact from "./pages/contact/Contact";
+import Industries from "./pages/industries/Industries";
+import Services from "./pages/services/Services";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="about" element={<About />} />
+          <Route path="insights" element={<Insights />} />
+          <Route path="careers" element={<Careers />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="industries" element={<Industries />} />
+          <Route path="services" element={<Services />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
