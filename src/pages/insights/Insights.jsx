ea7a4188d7f0347cfc4b7f7f1e6eae67b6a917b3
taotara <taotara@yahoo.com>
2024-01-08 09:54:54 +0100

    category filter implimented

--- src/pages/insights/Insights.jsx
+++ src/pages/insights/Insights.jsx
@@ -4,91 +4,23 @@ import Header from "../../layouts/header/Header";
 import PageTitle from "../../layouts/components/pageTitle/PageTitle";
 import InsightsBg from "/title/insights.jpg";
 import dayjs from "dayjs";
+import Blogs from "../../data/insights.json";
+import { Link } from "react-router-dom";
+import Select from 'react-select';
+import { useState } from "react";
 
 function Insights() {
-  const data = [
-    {
-      id: 1,
-      title: "Modern networks: How to fast track competitive advantage",
-      category: "Cloud",
-      image: "/insights/network.jpg",
-      excerpt:
-        "Network modernization can yield greater business resiliency and cost efficiency, creating a ripple effect of innovation.",
-      date: "2024-01-6",
-    },
-    {
-      id: 2,
-      title: "Modern networks: How to fast track competitive advantage",
-      category: "Oil & Gas",
-      image: "/insights/oil-gas.jpg",
-      excerpt:
-        "Network modernization can yield greater business resiliency and cost efficiency, creating a ripple effect of innovation.",
-      date: "2023-12-2",
-    },
-    {
-      id: 3,
-      title: "Modern networks: How to fast track competitive advantage",
-      category: "Automation",
-      image: "/insights/automation.jpg",
-      excerpt:
-        "Network modernization can yield greater business resiliency and cost efficiency, creating a ripple effect of innovation.",
-      date: "2023-09-25",
-    },
-    {
-      id: 4,
-      title: "Modern networks: How to fast track competitive advantage",
-      category: "Telecoms",
-      image: "/insights/5g.jpg",
-      excerpt:
-        "Network modernization can yield greater business resiliency and cost efficiency, creating a ripple effect of innovation.",
-      date: "2023-10-25",
-    },
-    {
-      id: 5,
-      title: "Modern networks: How to fast track competitive advantage",
-      category: "Agriculture",
-      image: "/insights/barns.jpg",
-      excerpt:
-        "Network modernization can yield greater business resiliency and cost efficiency, creating a ripple effect of innovation.",
-      date: "2023-01-25",
-    },
-    {
-      id: 6,
-      title: "Modern networks: How to fast track competitive advantage",
-      category: "Automation",
-      image: "/insights/automation.jpg",
-      excerpt:
-        "Network modernization can yield greater business resiliency and cost efficiency, creating a ripple effect of innovation.",
-      date: "2019-01-25",
-    },
-    {
-      id: 7,
-      title: "Modern networks: How to fast track competitive advantage",
-      category: "Agriculture",
-      image: "/insights/checken.jpg",
-      excerpt:
-        "Network modernization can yield greater business resiliency and cost efficiency, creating a ripple effect of innovation.",
-      date: "2019-01-25",
-    },
-    {
-      id: 8,
-      title: "Modern networks: How to fast track competitive advantage",
-      category: "Finance",
-      image: "/insights/finance.jpg",
-      excerpt:
-        "Network modernization can yield greater business resiliency and cost efficiency, creating a ripple effect of innovation.",
-      date: "2019-01-25",
-    },
-    {
-      id: 9,
-      title: "Modern networks: How to fast track competitive advantage",
-      category: "Automation",
-      image: "/insights/auto-program.jpg",
-      excerpt:
-        "Network modernization can yield greater business resiliency and cost efficiency, creating a ripple effect of innovation.",
-      date: "2019-01-25",
-    },
-  ];
+
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const categories = Array.from(
+      new Set(Blogs.map((res) => res.category) )
+    )
+
+    const categoryOptions = categories.map((category) =>({
+      value : category,
+      label : category
+    }))
+    // const filterBlogs = selectedCategory ? Blogs.filter((res) => res.category === selectedCategory.value):res;
 
   return (
     <div>
@@ -97,31 +29,42 @@ function Insights() {
       <div className="insights">
         <div className="container">
           <h2 className="h2-title">Explore Our Latest Insights</h2>
+          <div>
+            <select 
+              options = {categoryOptions}
+              isClearable
+              aria-placeholder="Select a category"
+              onChange={(selectOption) => setSelectedCategory(selectOption)}
+              value ={selectedCategory}
+            />
+          </div>
           <div className="row">
-            {data.map((d) => (
-              <div className="col-sm col-md-4">
+            { Blogs && Blogs.map(blog => {
+              return (
+                <div className="col-sm col-md-4" key={blog.id}>
                 <div className="card">
                   <div className="card-image">
-                    <img src={d.image} alt="Insights image" />
+                    <img src={blog.image} alt="{blog.title}" />
                     <div className="card-category">
-                      <h5 className="category-text">{d.category}</h5>
+                      <h5 className="category-text">{blog.category}</h5>
                     </div>
                   </div>
                   <div className="card-date">
-                    <div className="day">
-                    {dayjs(d.date).format('DD')}
+                    <div className="card-date-top">
+                      <div className="day">{dayjs(blog.date).format("DD")}</div>
+                    <div className="myr">{dayjs(blog.date).format("MMM, YY")}</div>
                     </div>
-                    <div className="myr">
-                    {dayjs(d.date).format('MMM, YY')}
-                    </div>
+                    <div className="triangle-topleft"></div>
                   </div>
+                  
                   <div className="card-info">
-                    <h5>{d.title}</h5>
-                    <p>{d.excerpt}</p>
+                    <Link to="/"><h5>{blog.title}</h5>
+                    <p>{blog.excerpt}</p></Link>
                   </div>
                 </div>
               </div>
-            ))}
+              )
+            })}
           </div>
         </div>
       </div>
